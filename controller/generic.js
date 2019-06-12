504b128926dd3d6d8c3ef8ccc6940e11bbb5ab73
Rishab Bahal <rishabbahal@gmail.com>
2019-06-12 17:53:59 +0530

    Pagination done!

--- controller/generic.js
+++ controller/generic.js
@@ -1,9 +1,28 @@
 const Doc=require('./../model/docs')
 
+const ITEMS_PER_PAGE=9; 
+
 exports.getSubjectDocs=(req,res,next)=>{
     const subject=req.params.subjectName;
+    const page=req.params.page;
+    var totalPages;
+
     Doc.find({subject: subject,authorized: true})
+    .count()
+    .then(totalDocs=>{
+        totalPages=parseInt(totalDocs/ITEMS_PER_PAGE);
+        if((totalDocs%ITEMS_PER_PAGE)>0)
+        {
+            totalPages++;
+        }
+        return Doc.find({subject: subject,authorized: true})
+        .skip((page-1)*ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE)
+    })
     .then(docsArray=>{
-        res.status(200).json({docsArray: docsArray})
+        res.status(200).json({
+            docsArray: docsArray,
+            totalPages: totalPages
+        })
     })
 }
--- routes/generic.js
+++ routes/generic.js
@@ -4,7 +4,7 @@ const router=express.Router()
 const genController=require('./../controller/generic')
 
 // GET /generic/ece/embeddedSystems
-router.get('/ece/:subjectName',genController.getSubjectDocs)
+router.get('/ece/:subjectName/:page',genController.getSubjectDocs)
 
 
 module.exports=router
